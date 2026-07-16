c0b2d0bd74206a6f6f74632b77af6e9efe7ae2da
AcreroYT <126112250+AcreroYT@users.noreply.github.com>
2026-07-16 15:39:02 +0200

    Update 1.0 ban script.lua

--- 1.0 ban script/1.0 ban script.lua
+++ 1.0 ban script/1.0 ban script.lua
@@ -4,7 +4,7 @@ local banList = {123, 456, 789}
 -- KILL THEM
 game["Players"].PlayerAdded:Connect(function(player)
   for i, v in banList do
-    if player.UserID == i then
+    if player.UserID == v then
       player:kick("You've been a bad boy!")
     end
   end
